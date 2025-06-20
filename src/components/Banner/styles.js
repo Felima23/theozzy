0618db38905edbdd12425303d7ee0a5d37202282
Felima23 <felipedecastroelima669@gmail.com>
2025-06-20 12:53:37 -0300

    alteracao banner

--- src/components/Banner/styles.js
+++ src/components/Banner/styles.js
@@ -3,11 +3,16 @@ import styled from 'styled-components'
 export const H1 = styled.h1`
     font-weight: 900;
     color: yellow;
-    letter-spacing:3px;
+    letter-spacing:2px;
     font-family: "Anton", sans-serif;
     text-shadow: 2px 2px 4px #000;
-    font-size: clamp(1.9rem, 8vw, 5rem); /* ajusta conforme a largura da tela */
+    font-size: clamp(24px, 6vw, 60px); /* Responsivo */
+    line-height: 1.2;
     span{
         color:#fff;
     }
+
+    @media (max-width: 480px) {
+    text-align:center;
+  }
 `;
