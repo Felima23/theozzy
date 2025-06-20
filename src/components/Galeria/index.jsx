65005c9f5ae67b2f8a81119c376ec189a59a65f1
Felima23 <felipedecastroelima669@gmail.com>
2025-06-20 12:35:50 -0300

    alteracao galeria

--- src/components/Galeria/index.jsx
+++ src/components/Galeria/index.jsx
@@ -10,17 +10,14 @@ import imagem7 from "../../assets/IMG/imagem7.jpg"
 
 
 function Galeria (){
+     const imagens = [imagem1, imagem2, imagem3, imagem4, imagem5, imagem6, imagem7];
     return(
         <>  <H1>NOSSA GALERIA</H1>
             <div className="galeria-container">
                 <div className="galeria-faixa">
-                    <IMG src={imagem1} alt="Imagem 1" />
-                    <IMG src={imagem2} alt="Imagem 2" />
-                    <IMG src={imagem3} alt="Imagem 3" />
-                    <IMG src={imagem4} alt="Imagem 4" />
-                    <IMG src={imagem5} alt="Imagem 5" />
-                    <IMG src={imagem6} alt="Imagem 6" />
-                    <IMG src={imagem7} alt="Imagem 7" />
+                    {imagens.concat(imagens).map((img, index) => (
+                        <IMG key={index} src={img} alt={`Imagem ${index + 1}`} />
+                    ))}
                 </div>
             </div>
         </>
